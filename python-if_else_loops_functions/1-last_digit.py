ee30149f9f29a1acf52757a6680c2706bedca1fe
seeyou7 <>
2023-09-26 04:17:03 -0700

    check if the random num is negative or positive acording to a specific condition

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    mod = (-1) * number % 10
+    mod = (-1) * mod
+
+else:
+    mod = number % 10
+
+if mod == 0:
+    print(f"Last digit of {number} is {mod} and is 0")
+elif mod > 5:
+    print(f"Last digit of {number} is {mod} and is greater than 5")
+else:
+    print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
