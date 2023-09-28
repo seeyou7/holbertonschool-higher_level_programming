0d51fa06d9776444ffe7df8343c82542534cf90e
seeyou7 <>
2023-09-28 06:59:15 -0700

    function that prints the numbers from 1 to 100 separated by a space and replace 3 5 by fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print(f"FizzBuzz".format(i), end=" ")
+        elif i % 3 == 0:
+            print(f"Fizz".format(i), end=" ")
+        elif i % 5 == 0:
+            print(f"Buzz".format(i), end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
+
+        
+
