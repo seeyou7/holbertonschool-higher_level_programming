a1f012afb22ccd441fb91e1f7261c0b97a09183c
seeyou7 <>
2023-09-28 03:33:58 -0700

    fix print .format error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,9 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        # Check if the character is a lowercase letter (ASCII values between 97 and 122)
+def uppercase(input_str):
+    result = ""
+    for char in input_str:
         if 97 <= ord(char) <= 122:
-            # Convert the lowercase letter to uppercase using ASCII manipulation
             uppercase_char = chr(ord(char) - 32)
-            print(uppercase_char, end="")
+            result += uppercase_char
         else:
-            # If the character is not a lowercase letter, print it as is
-            print(char, end="")
-    # Print a newline character after processing the entire string
-    print()
-
+            result += char
+    print("{}".format(result))
