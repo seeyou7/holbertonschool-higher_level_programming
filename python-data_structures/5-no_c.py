3ab5d6e4095a4e75012a2a8c37bfa0ede535222f
seeyou7 <>
2023-10-03 06:32:15 -0700

    fix pycodestyle error

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     rm_char = "cC"
-    modify_str = ''.join([char for char in my_string if char not in  rm_char])
+    modify_str = ''.join([char for char in my_string if char not in rm_char])
     return modify_str
