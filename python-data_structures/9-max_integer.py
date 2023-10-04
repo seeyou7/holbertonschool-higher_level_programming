df6e03cd041f1b8e65f10b4b2f6964da4562edda
seeyou7 <>
2023-10-04 06:35:02 -0700

    fix py code style

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    big_num = my_list[0]
     if my_list == "":
         return None
+    big_num = my_list[0]
     for i in my_list[1:]:
         # iterate starting from the seconde element
         if i > big_num:
             big_num = i
-            return big_num
+    return big_num
