9fdc6d22d05b4a76bb4093bd7a705267286c66b8
seeyou7 <>
2023-10-04 06:01:28 -0700

    fix pycode errors

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        if len(sentence) == "":
-             return (0, None)
-        return (len(sentence), sentence[0])
-
-        
+    if len(sentence) == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
