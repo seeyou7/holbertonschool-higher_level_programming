08ba79862b5afdc5f56041beff9657c1c6e7d8e7
seeyou7 <>
2023-10-06 02:19:48 -0700

    fix pycode style

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,9 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    high_val = max(a_dictionary.values())
-    for key, value in a_dictionary.items():
-        if value == high_val:
-            return high_val
+    else:
+
+        high_val = max(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if value == high_val:
+                return high_val
