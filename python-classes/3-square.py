a4c93bf0a1fada4d470ca5bf418746da0cb4b3bb
seeyou7 <>
2023-10-10 07:15:23 -0700

    class Square that defines a square using Public instance method: def area(self)

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''this module define an empty class'''
+
+
+class Square:
+    '''
+    this class represent a square
+    '''
+    def area(self):
+        return self.__size ** 2
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
