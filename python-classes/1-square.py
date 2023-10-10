c9aae3baaba62b2e1a4f844c59c2be913c9a54d7
seeyou7 <>
2023-10-10 03:52:47 -0700

    diffine a Private instance attribute: size for class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''this module define an empty class'''
+
+
+class Square:
+    '''
+    this class represent a square
+    '''
+    def __init__(self, size):
+        self.__size = size
+        pass
