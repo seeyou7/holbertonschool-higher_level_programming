9bddca9bd22e210044f2d83d67bb5884324b6c93
seeyou7 <>
2023-10-11 05:55:36 -0700

    class Square that defines a square by: (based on 5-square.py)

---
+++ python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''This module defines the Square class'''
+
+
+class Square:
+    '''
+    This class represents a square
+    '''
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print("_" * self.position[0] + "#" * self.size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 integers")
+        elif not all(isinstance(i, int) for i in value):
+            raise TypeError("position must be a tuple of 2 integers")
+        elif any(i < 0 for i in value):
+            raise ValueError("both elements of position must be >= 0")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
