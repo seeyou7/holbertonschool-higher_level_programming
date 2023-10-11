1f3f5c81c4bdaf4d8ad9a3aeef5b30e8a18b691b
seeyou7 <>
2023-10-11 06:09:16 -0700

    fix pycode errors

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-'''This module defines the Square class'''
+'''defines a square'''
 
 
 class Square:
-    '''
-    This class represents a square
-    '''
+
+    """Define a Square"""
 
     def __init__(self, size=0, position=(0, 0)):
+        '''Instantiation with optional size'''
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        '''property def size'''
         return self.__size
 
     @size.setter
     def size(self, value):
+        '''setter def size'''
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
     @property
     def position(self):
@@ -30,19 +31,25 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 integers")
-        else:
-            self.__position = value
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if not all(isinstance(num, int) and num >= 0 for num in value):
+            raise ValueError('position must be a tuple of 2 positive integers')
+        self.__position = value
 
     def area(self):
-        return self.__size ** 2
+        '''returns the current square area'''
+        return self.__size * self.__size
 
     def my_print(self):
         if self.size == 0:
             print()
         else:
-            for _ in range(self.position[1]):
+            for _ in range(self.__position[1]):
                 print()
-            for _ in range(self.size):
-                print("_" * self.position[0] + "#" * self.size)
+            for _ in range(self.__size):
+                for _ in range(self.__position[0]):
+                    print(" ", end="")
+                for _ in range(self.__size):
+                    print("#", end="")
+                print()
