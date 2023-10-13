630fa8a0735c869355abfe34a883066244d8fa28
seeyou7 <>
2023-10-13 08:44:17 -0700

    function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" function that prints a text """
+
+
+def text_indentation(text):
+    """ print texte """
+
+    punctuation = [".", "?", ":"]
+    value = False
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    for i in text:
+        if i in punctuation:
+            print(i)
+            print()
+            value = True
+        else:
+            if value is False:
+                print(i, end="")
+            else:
+                if i != " ":
+                    print(i, end="")
+                    value = False
