a036b860ee7df5671c76ba9f86f3f5653418bd30
seeyou7 <>
2023-10-13 05:06:11 -0700

    fix pycode error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,7 +12,7 @@
 def matrix_divided(matrix, div):
     """ Divides a matrix and returns a new matrix """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a (list of lists) of integers/floats")
 
     for row in matrix:
         if not isinstance(row, list):
