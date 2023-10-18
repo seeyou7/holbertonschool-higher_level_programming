0d49a580bb2b4cd796819c649be082e4cd12a57c
seeyou7 <>
2023-10-18 03:10:56 -0700

    fix pycode error

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+   func that return true if obj is instance of a class
+"""
+
+
 def is_same_class(obj, a_class):
-    return type(obj) is a_class 
+    """ check the same class """
+    return type(obj) is a_class
