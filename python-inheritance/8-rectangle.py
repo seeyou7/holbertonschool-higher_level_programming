26ab28ff6281c4d3acc15a503ab113863df30163
seeyou7 <>
2023-10-18 10:34:06 -0700

    fix pycode errors

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""Write an empty class BaseGeometry."""
+""""Rectangle heritage"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """base geometry"""
-    def area(self):
-        """ area define"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 class Rectangle(BaseGeometry):
-    """ child class """
+    """rectangle"""
     def __init__(self, width, height):
         super().integer_validator("width", width)
-        super().integer_validator("height", height )
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-
