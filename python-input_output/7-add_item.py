7d65b5706a02d1df2fa7ff4c2dfba5a32fa9c2d9
seeyou7 <>
2023-10-20 10:44:29 -0700

    function that returns the dictionary description with simple data structure

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" import all module """
+import sys
+
+
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+def lists(argument):
+    """ add argument"""
+    try:
+        value = load("add_item.json")
+    except FileNotFoundError:
+        value = []
+
+    value += argument
+    save(value, "add_item.json")
+
+
+argument = sys.argv[1:]
+lists(argument)
