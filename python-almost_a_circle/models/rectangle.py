f733e2d76041e45a925412dd03874d0aec30d819
seeyou7 <>
2023-10-24 07:09:47 -0700

    overriding str methode

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,6 @@ class Rectangle(Base):
             chong += (" " * self.x)
             chong += ("#" * self.width) + "\n"
         print(chong, end="")
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -  {self.width}/{self.height}"
