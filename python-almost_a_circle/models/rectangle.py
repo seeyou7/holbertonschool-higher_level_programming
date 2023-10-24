1d1aa68e3fbff1ea592b6251f600eac1e2b9b27e
seeyou7 <>
2023-10-24 07:43:00 -0700

    fix the return limited char by line

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -107,4 +107,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """ override using str """
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return (
+                f"[Rectangle] ({self.id}) "
+                f"{self.x}/{self.y} - {self.width}/{self.height}"
+        )
