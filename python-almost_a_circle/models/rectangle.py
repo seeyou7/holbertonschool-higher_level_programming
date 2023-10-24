1cb9ee6255b93d316a6aa2293d1ee3988e7ca973
seeyou7 <>
2023-10-24 12:33:48 -0700

    update rectangle using kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -111,7 +111,7 @@ class Rectangle(Base):
                 f"{self.x}/{self.y} - {self.width}/{self.height}"
         )
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """" update the attribute of recta """
         if args:
             if len(args) >= 1:
@@ -124,3 +124,6 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) >= 5:
                 self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
