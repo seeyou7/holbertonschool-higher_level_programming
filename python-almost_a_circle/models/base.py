6a545ed10860af67f38ee64f1ec02dd7f99a78da
seeyou7 <>
2023-10-27 06:46:58 -0700

    add static methode and class methode

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,7 +33,34 @@ class Base:
         list_dict = []
         filename = cls.__name__ + ".json"
         with open(filename, "w", encoding="utf-8") as file:
-            """ create and convert list_dict to list of dictionaries """
+            """ create and convert list_dict to list of dict """
             for i in list_objs:
                 list_dict.append(i.to_dictionary())
             file.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ return the list of the JSON string representation json_string """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            obj = cls(2, 3)
+        else:
+            obj = cls(3)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        """ returns a list of instances """
+        filename = cls.__name__ + ".json"
+        if not path.exists(filename):
+            return []
+        with open(filename, "r", encoding="utf-8") as file:
+            obj = Base.from_json_string(file.read())
+        list = [cls.create(**dict) for dict in obj]
+        return list
