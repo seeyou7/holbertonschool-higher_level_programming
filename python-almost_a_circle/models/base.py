b6368cf5580c48fa2c79f528b46fb13e83135b9f
seeyou7 <>
2023-10-27 06:37:47 -0700

    addig public method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,16 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ write list_ objs in filename using json """
+        if list_objs is None:
+            list_objs = []
+        list_dict = []
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as file:
+            """ create and convert list_dict to list of dictionaries """
+            for i in list_objs:
+                list_dict.append(i.to_dictionary())
+            file.write(Base.to_json_string(list_dict))
