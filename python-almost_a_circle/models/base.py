34049c5af2f9bd4e863f1ff155c9cb35b9c2b3cc
seeyou7 <>
2023-10-27 06:16:44 -0700

    add condition to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ static methoode that return representation of list dict """
         if list_dictionaries is None:
