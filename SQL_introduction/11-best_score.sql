465f4101fd9371a89172f1fcdd17fcf3ece5681b
seeyou7 <5764@holbertonstudents.com>
2023-11-22 17:26:35 +0100

    make it one line

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
  -- lists all records with a score >= 10 in the table second_table of the database
-SELECT DISTINCT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+SELECT DISTINCT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
