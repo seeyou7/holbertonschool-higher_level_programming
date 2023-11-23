a89ca1ccb57b7eb83f78cbce0a69a511131760ac
seeyou7 <5764@holbertonstudents.com>
2023-11-23 13:04:25 +0100

    computes the score average of all records in table

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- computes the score average of all records in the table second_table of the database 
-SELECT AVG(score) AS average FROM second_table;git add
+SELECT AVG(score) AS average FROM second_table;
