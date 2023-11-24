fe5c1b528283a149ef189edba6ef1aa693fb84de
seeyou7 <5764@holbertonstudents.com>
2023-11-24 16:30:38 +0100

    create table with foreign key that refer to primary key

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,10 @@
 --  creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create tabel with freign key
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+-- state_id column in the current table is a foreign key that refers to the id column in the states with primary key
+FOREIGN KEY (state_id) REFERENCES states(id)
+);
