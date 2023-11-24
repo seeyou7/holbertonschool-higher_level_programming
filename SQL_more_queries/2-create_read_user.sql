fae2c4dd76693d74edc7bc00899b1d07f0a62252
seeyou7 <5764@holbertonstudents.com>
2023-11-24 14:37:21 +0100

    rmove second comment

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
-/* creates the database hbtn_0d_2 and the user user_0d_2
-select privilege= (delete, from, where, sum, avg )
-*/
+-- select privilege= delete, from, where, sum, avg 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
