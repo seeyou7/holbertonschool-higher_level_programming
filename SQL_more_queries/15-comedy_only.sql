e4b2455032178dcac1658d53e3e807fa044d2636
seeyou7 <5764@holbertonstudents.com>
2023-11-26 16:46:25 +0100

    test push for token

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
--- listing all Comedy shows in the database hbtn_0d_tvshows
+-- listing all Comedy shows in the database hbtn_0d_tvshows .
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
