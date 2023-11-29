45a8a98b13c2ddf82a1d34c56bf457ef86abb5cb
seeyou7 <>
2023-11-29 06:12:13 -0800

    delete asc order

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3])
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM states WHERE name = '{}'\
-                ORDER BY id ASC".format(sys.argv[4]))
+                ORDER BY id ".format(sys.argv[4]))
         for row in cur.fetchall():
             print(row)
 
