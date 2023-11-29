b0641a8a6e8e2dfd05ec0ca20ab22c56fc1b7a4f
seeyou7 <5764@holbertonstudents.com>
2023-11-29 12:51:23 +0100

    fix error of putting py code in sql file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
-"""
-    script taking 3 arguments mysql username, mysql password, database name
-    lists all states from the database hbtn_0e_0_usa sorted by states_id asc
-    using module MySQLdb
-    connect to MySQL server running on localhost at port 3306
-    code dont be executed when imported
-"""
+""" lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        sys.exit("Usage : python script.py <username> <password> <database>")
+        sys.exit("Usage: file_name, username, password, db")
 
-    """ connection to the database """
-    db = MySQLdb.connect(host="localhost", port=3306, charset='utf8',
-                         user=sys.argv[1], passwd=sys.argv[2],
-                         database=sys.argv[3])
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    """ create a cursor to execute sql command  """
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        for row in cur.fetchall():
+            print(row)
 
-    """ retrieve all the rows of the table states"""
-    lines = cur.fetchall()
-    for line in lines:
-        print(line)
-
-    """  close the cursor and the connection """
-    cur.close()
-    db.close()
+    conn.close()
