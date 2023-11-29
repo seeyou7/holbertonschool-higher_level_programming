3d534fc786da939a88f6e5c9f2944d5e4ab365e5
seeyou7 <5764@holbertonstudents.com>
2023-11-29 11:08:37 +0100

    change script by adding try and exept

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
-'''listing all states from the database hbtn_0e_0_usa'''
+"""Script to list all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
+    # Check if the correct number of arguments is provided
+    if len(argv) != 4:
+        print("Usage: {} username password database".format(argv[0]))
+        exit(1)
 
-    with conn.cursor() as cur:
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        for row in cur.fetchall():
-            print(row)
+    # Extracting command-line arguments
+    username, password, database = argv[1], argv[2], argv[3]
 
-    conn.close()
+    try:
+        # Establishing a connection to the database
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+        # Creating a cursor object to execute SQL queries
+        with connection.cursor() as cursor:
+            # Executing the SQL query to select all states
+            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+            # Fetching and printing the results
+            states = cursor.fetchall()
+            for state in states:
+                print(state)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+
+    finally:
+        # Closing the database connection
+        if connection:
+            connection.close()
