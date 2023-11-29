906e88243f6ddc2492ed8f05fda6bbd82fb5771e
seeyou7 <>
2023-11-29 05:05:00 -0800

    lists all states with a name starting with (upper N) from the database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" write a script that lists all states from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit("Usage: file_name, username, password, db")
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM states WHERE \
+                    name LIKE 'N%' ORDER BY states.id ASC")
+        for row in cur.fetchall():
+            if ('N' in row[1]):
+                print(row)
+    conn.close()
+
