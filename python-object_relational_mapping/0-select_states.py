5eaf6546b6361e53809fe9e3240042073ae70092
seeyou7 <5764@holbertonstudents.com>
2023-11-29 11:58:44 +0100

    lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,42 +1,23 @@
 #!/usr/bin/python3
-"""Script to list all states from the database hbtn_0e_0_usa"""
-
+""" write a script that lists all states from the database hbtn_0e_0_usa """
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(argv) != 4:
-        print("Usage: {} username password database".format(argv[0]))
-        exit(1)
+    if len(sys.argv) != 4:
+        sys.exit("Usage: file_name, username, password, db")
 
-    # Extracting command-line arguments
-    username, password, database = argv[1], argv[2], argv[3]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    try:
-        # Establishing a connection to the database
-        connection = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        for row in cur.fetchall():
+            print(row)
 
-        # Creating a cursor object to execute SQL queries
-        with connection.cursor() as cursor:
-            # Executing the SQL query to select all states
-            cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-            # Fetching and printing the results
-            states = cursor.fetchall()
-            for state in states:
-                print(state)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-
-    finally:
-        # Closing the database connection
-        if connection:
-            connection.close()
+    conn.close()
