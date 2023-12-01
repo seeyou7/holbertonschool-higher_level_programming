aed16f31d4e564320fe418a99b30a0385728816d
seeyou7 <>
2023-12-01 08:07:16 -0800

    fix pycode error

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,9 +9,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username, password, db, state_name = \
-    sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
+    username, password, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db))
     Session = sessionmaker(bind=engine)
