e570f028d76f9007823da66a0dd6e6892b02f4dd
seeyou7 <5764@holbertonstudents.com>
2023-12-01 14:01:55 +0100

    prints the first State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that prints the first State object from the database hbtn_0e_6_usa"""
+"""prints the first State object from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
